b58baf76e998f0028654893350ac495e46fd7a4a
Wisit.o <wisit.o@siamwinery.com>
2023-11-01 21:57:16 +0700

    initialized commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>This is my first Typescript react app with vite</h1>
+        <p>
+          as you know, I am a very newbie front-end developer and this website is also treat as component lab for my other project too.
+        </p>
+        <p>
+          thanks for your visiting this website, let enjoy !!!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
